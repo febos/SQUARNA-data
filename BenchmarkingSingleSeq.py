cd8dc40e5256d6eeb474c252ebc75678da7ef9c2
Eugene Baulin <efbaulin@gmail.com>
2026-05-22 00:20:02 +0200

    pseudoknot benchmark

--- BenchmarkingSingleSeq.py
+++ BenchmarkingSingleSeq.py
@@ -707,26 +707,8 @@ if __name__ == "__main__":
     dtst  = "SRtrain150"
     tl    = "CONTRAfold"
 
-    for dataset, tool in (("SRtest",  "SQUARNA"),
-                          ("SRtest",  "SQUARNA5"),
-                          ("SRtest",  "SQUARNAE"),
-                          ("SRtest",  "SQUARNAE5"),
-                          ("SRtest",  "SQUARNAEnb"),
-                          ("SRtest",  "SQUARNAEnb5"),
-                          ("SRtest",  "SQUARNAG"),
-                          ("SRtest",  "SQUARNAG5"),
-                          ("SRtest",  "SQUARNAGnb"),
-                          ("SRtest",  "SQUARNAGnb5"),
-                          ("SRtest",  "SQUARNAH"),
-                          ("SRtest",  "SQUARNAH5"),
-                          ("SRtest",  "SQUARNAHnb"),
-                          ("SRtest",  "SQUARNAHnb5"),
-                          ("SRtest",  "SQUARNAN"),
-                          ("SRtest",  "SQUARNAnb"),
-                          ("SRtest",  "SQUARNAnb5"),
-                          ("SRtest",  "SQUARNANnb"),
-                          ("SRtest",  "SQUARNArfam"),
-                          ("SRtest",  "SQUARNArfam5"),
+    for dataset, tool in (("pseudobasepp2",  "SQUARNArfam"),
+                          ("pseudobasepp2",  "SQUARNArfam5"),
                           ):
         if NL:
             dataset += "NL"
